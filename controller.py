f1285991e703bb50b94a174600ece0f267ce3e61
Bryan Morrison <skeeter_mcbee@live.com>
2019-02-01 18:42:47 -0800

    Update controller.py

--- controller.py
+++ controller.py
@@ -226,9 +226,9 @@ if commandArgs.type == 'l298n':
 #Test if user
 if commandArgs.type == "pololu":
     try:
-	from pololu_drv8835_rpi import motors, MAX_SPEED
+    from pololu_drv8835_rpi import motors, MAX_SPEED
     except ImportError:
-	print("You need to install drv8835-motor-driver-rpi")
+        print("You need to install drv8835-motor-driver-rpi")
         print("Please install drv8835-motor-driver-rpi for python and restart this script.")
         print("To install: cd /usr/local/src && sudo git clone https://github.com/pololu/drv8835-motor-driver-rpi")
         print("cd /usr/local/src/drv8835-motor-driver-rpi && sudo python setup.py install")
